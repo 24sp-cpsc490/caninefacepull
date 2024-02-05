7355d8af46c88f11a3d49d6861036b3be4ee97d7
hunter-hues <98989668+hunter-hues@users.noreply.github.com>
2024-02-04 23:37:31 -0800

    Hunter Hughes Changes HW
    changed the 2 mains

--- main.cpp
+++ main.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World, I changed it";
+    cout << "Hello World, I changed it" << endl;
+    cout << "I wanted to add a ascii text image but it wouldnt let me cout it as it was so I didnt wanna put in that much time to fix it.";
+    
+
 
 
     return 0;
