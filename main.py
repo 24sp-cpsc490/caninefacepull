c449e3c3251caf007843ac7abfd636bab171982e
Codie Tamida <codietamida@gmail.com>
2024-02-15 09:19:48 -0800

    fixed merge conflicts

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-def main():
-    print("Hello World!")
-    print("I like your dog.")    
-    print("Test")
-    print("pp caca")
-    print("lol this class is great")
 
-    print("This is an original change!")
-    print("More original changes!!!")
+print("Hello World!")
 
-if __name__ == '__main__':
-    main()
+print("hiiiii this is a test")
+
+print("Really important changes")
